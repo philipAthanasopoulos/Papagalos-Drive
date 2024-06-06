53db41f52cc5807100a04eea2849b42e1dad6eee
philipAthanasopoulos <filipposfivos@gmail.com>
2024-06-06 21:41:41 +0300

    Initialized repository

---
+++ api/schoolio-api/src/main/java/org/example/schoolioapi/SchoolioApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.schoolioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolioApiApplication.class, args);
+	}
+
+}
---
+++ api/schoolio-api/src/test/java/org/example/schoolioapi/SchoolioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.schoolioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
