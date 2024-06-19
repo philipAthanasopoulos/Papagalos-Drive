09810865d539a3855d81ca142f5eff79557b3ea0
philipAthanasopoulos <filipposfivos@gmail.com>
2024-06-19 10:40:51 +0300

    connected mongo databsae

--- api/schoolio-api/src/main/java/org/example/schoolioapi/SchoolioApiApplication.java
+++ api/schoolio-api/src/main/java/org/example/schoolioapi/SchoolioApiApplication.java
@@ -3,12 +3,13 @@ package org.example.schoolioapi;
 import org.example.schoolioapi.domain.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class})
 public class SchoolioApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolioApiApplication.class, args);
 	}
