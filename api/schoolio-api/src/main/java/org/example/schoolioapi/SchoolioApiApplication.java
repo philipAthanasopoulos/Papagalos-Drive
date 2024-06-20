36754150b8abda09c7845d5c69db513595d57ef5
philipAthanasopoulos <filipposfivos@gmail.com>
2024-06-20 18:03:26 +0300

    successfully connected to mongo atlas database removed previous mongo config

--- api/schoolio-api/src/main/java/org/example/schoolioapi/SchoolioApiApplication.java
+++ api/schoolio-api/src/main/java/org/example/schoolioapi/SchoolioApiApplication.java
@@ -1,6 +1,9 @@
 package org.example.schoolioapi;
 
+import org.example.schoolioapi.domain.Address;
+import org.example.schoolioapi.domain.Gender;
 import org.example.schoolioapi.domain.Student;
+import org.example.schoolioapi.repository.StudentRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -8,9 +11,33 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(exclude = {MongoAutoConfiguration.class})
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@SpringBootApplication()
 public class SchoolioApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolioApiApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner runner(StudentRepository repository){
+		return args -> {
+			Student student  = new Student(
+					"John",
+					"Doe",
+					"johnDoe@gmail.com",
+					Gender.MALE,
+					new Address(
+							"a","b","c"
+					),
+					List.of("Chemistry"),
+					BigDecimal.ONE,
+					LocalDateTime.now()
+			);
+
+			repository.insert(student);
+		};
 	}
 }
