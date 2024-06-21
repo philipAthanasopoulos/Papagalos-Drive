01b0f4ac542e4f613faf84b622e1ae4068e952d6
philipAthanasopoulos <filipposfivos@gmail.com>
2024-06-21 16:00:17 +0300

    Refactor StudentCards component to remove unused imports and interfaces

--- frontend/schoolio-web-app/src/components/StudentCards.tsx
+++ frontend/schoolio-web-app/src/components/StudentCards.tsx
@@ -1,11 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-interface Address {
-    country: string;
-    city: string;
-    postalCode: string;
-}
 
 interface User {
 
