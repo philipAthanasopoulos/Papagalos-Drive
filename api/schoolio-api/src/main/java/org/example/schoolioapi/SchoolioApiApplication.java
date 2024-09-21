a142fc17e18e0e736a978d6fcd3829b95980a2c2
philipAthanasopoulos <filipposfivos@gmail.com>
2024-09-21 19:58:25 +0300

    test new jarfile

--- api/schoolio-api/src/main/java/org/example/schoolioapi/SchoolioApiApplication.java
+++ api/schoolio-api/src/main/java/org/example/schoolioapi/SchoolioApiApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication()
 @EnableCaching
 @EnableScheduling
-//@EnableWebSecurity
 public class SchoolioApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(SchoolioApiApplication.class, args);
--- api/schoolio-api/src/main/java/org/example/schoolioapi/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-//package org.example.schoolioapi.repository;
-//
-//import org.example.schoolioapi.domain.User;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface UserRepository extends JpaRepository<User, Long> {
-//    User findByEmail(String email);
-//}
