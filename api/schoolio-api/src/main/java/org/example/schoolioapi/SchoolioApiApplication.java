645862efdfd1261dd5dcd668fc3464d0d348e86d
philipAthanasopoulos <filipposfivos@gmail.com>
2024-09-22 14:17:15 +0300

    test new jarfile

--- api/schoolio-api/src/main/java/org/example/schoolioapi/SchoolioApiApplication.java
+++ api/schoolio-api/src/main/java/org/example/schoolioapi/SchoolioApiApplication.java
@@ -1,14 +1,28 @@
 package org.example.schoolioapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.Arrays;
 
 @SpringBootApplication()
 @EnableCaching
 @EnableScheduling
 public class SchoolioApiApplication {
+
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    public void setEnvironment(Environment environment) {
+        System.out.println("Active Profiles: " + Arrays.toString(environment.getActiveProfiles()));
+        System.out.println("Property 'spring.datasource.url': " + environment.getProperty("spring.datasource.url"));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SchoolioApiApplication.class, args);
     }
